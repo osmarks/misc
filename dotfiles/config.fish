592b70c394ce193a712c9119d3f15bae2ccfd67b
osmarks <me@osmarks.net>
2025-07-28 11:22:10 +0100

    yet more dotfiles

---
+++ dotfiles/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    zoxide init fish | source
+    atuin init fish | source
+end
+if status is-login
+  and status is-interactive
+    # To add a key, set -Ua SSH_KEYS_TO_AUTOLOAD keypath
+    # To remove a key, set -U --erase SSH_KEYS_TO_AUTOLOAD[index_of_key]
+    keychain --eval $SSH_KEYS_TO_AUTOLOAD 2> /dev/null | source
+end
+#if status is-interactive
+#    
+#end
